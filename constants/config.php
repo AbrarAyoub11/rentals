dcf7c66e5a8431b0f0f2952029e71655b5ac620e
AbrarAyoub11 <abrarayoub011@gmail.com>
2023-08-22 15:17:29 +0530

    Initial Commit

---
+++ constants/config.php
@@ -0,0 +1,37 @@
+<?php
+//Website Settings
+$site_title = "Rentals";
+$about_site = "A free classified ads website for Cars, Phones, Electronics, Smartphones, Real estate, and everything else. Find what you're looking for or create your own ad for free!.";
+
+date_default_timezone_set('Asia/Kolkata');
+
+$currency = "&#8377;";
+$installation_path = "localhost/olx4";
+
+
+//Social Network Setings
+$facebook_link = "#";
+$twitter_link = "#";
+$instagram_link = "#";
+$googleplus_link = "#";
+
+//Database Settings
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "DB_RENTAL";
+
+//Contact Settings
+$site_email = "abrarayoub011@gmail.com";
+$site_phone = "+916005136313";
+$site_address = "Hyderpora Airport Road 190014";
+
+//SMTP Settings
+$smtp_host = '';
+$smtp_user = '';
+$smtp_pass = '';
+
+
+
+
+?>
